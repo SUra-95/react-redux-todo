e1ff89d4f59625466bb65865ce4251f82e1ab4e9
suranjan <suranjan@thesanmark.com>
2024-07-27 17:00:55 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Todos from './components/Todos';
+import AddTodo from './components/AddTodo'
+function App() {
+  return (
+    <div className="App">
+      <h1>This is Sample Text for Testing</h1>
+      <AddTodo/>
+      {/* <Todos/> */}
+    </div>
+  );
+}
+
+export default App;
